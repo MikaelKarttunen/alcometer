da017b0f9a888e8bc2be1e2036e86da23abcbb4d
Mikael Karttunen <n2kami00@students.oamk.fi>
2023-02-01 12:57:07 +0200

    Edits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React, { useState } from "react";
+
+const App = () => {
+  const [weight, setWeight] = useState(0);
+  const [gender, setGender] = useState("male");
+  const [beer, setBeer] = useState(0);
+  const [time, setTime] = useState(0);
+  const [result, setResult] = useState(0);
+
+  const calculateBloodAlcoholLevel = () => {
+    const litres = beer * 0.33;
+    const grams = litres * 8 * 4.5;
+    const burning = weight / 10;
+    const gramsLeft = grams - (burning * time);
+    let result;
+    if (gender === "male") {
+      result = gramsLeft / (weight * 0.7);
+    } else {
+      result = gramsLeft / (weight * 0.6);
+    }
+    result = result < 0 ? 0 : result;
+    setResult(result.toFixed(2));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="divi">
+      <h1>Alkometri</h1>
+      <div>
+        <label>Paino (kg):</label>
+        <input
+          type="number"
+          value={weight}
+          onChange={(e) => setWeight(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Sukupuoli:</label>
+        <select value={gender} onChange={(e) => setGender(e.target.value)}>
+          <option value="male">Mies</option>
+          <option value="female">Nainen</option>
+        </select>
+      </div>
+      <div>
+        <label>Kaljan määrä:</label>
+        <input
+          type="number"
+          value={beer}
+          onChange={(e) => setBeer(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Aika (tunnit):</label>
+        <input
+          type="number"
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+        />
+      </div>
+      <button onClick={calculateBloodAlcoholLevel}>Laske</button>
+      <div>
+        <h2>Tulos:</h2>
+        <p>{result}</p>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
